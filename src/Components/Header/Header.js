b134d6b955ff9cf828b765b2374a5b68ea8c7d36
Ella Horn <ellalouisahorn@yahoo.com>
2020-02-12 20:46:46 -0500

    updated header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,12 @@ const Header = () => {
   return (
     <header>
       <h1>Cookstagram</h1>
+      <p>
+        Are you tired of scrolling through your friends' vacations photos,
+        selfies, the (cute, sometimes not cute) photos of their children? Join
+        the social network dedicated to the one thing that really matters -
+        food, and how to make it.
+      </p>
     </header>
   );
 };
