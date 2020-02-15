0f22cd849b5905fc23b9089e31b4daca7ef72a62
Ella Horn <ellalouisahorn@yahoo.com>
2020-02-15 14:37:06 -0500

    added mock data

---
+++ src/MockData/MockRecipes.js
@@ -0,0 +1,135 @@
+export default {
+  recipes: [
+    {
+      user: "ellahorn",
+      recipename: "Pepper Steak",
+      picture:
+        "https://www.skinnytaste.com/wp-content/uploads/2009/07/Pepper-Steak-4-5.jpg",
+      ingredients: [
+        "12oz top round beef, trimmed",
+        "4 tsp plus 3tbsp soy sauce",
+        "1 tbsp rice wine",
+        "3 tsp cornstarch",
+        "1tbsp vegetable oil",
+        "1 large onion, sliced into thin strips",
+        "1 bell pepper, sliced into thin strips",
+        "1/2 tsp black pepper",
+        "crushed red pepper flakes, optional"
+      ],
+      instructions: [
+        "Slice beef into thin slices with the grain. Cut each strip across the grain about 1 inch long so you have small thin slices. Place in a bowl and add 4 tsp of soy sauce, 1 tbsp of rice wine, 1 tsp cornstarch and black pepper.",
+        "In a small bowl, mix 3 tbsp soy sauce, 1 tbsp water and 2 tsp cornstarch. Set aside.",
+        "Heat the wok on high heat. Swirl in 2 teaspoons of the oil and add the beef spreading evenly in the wok. Cook undisturbed for 20 seconds letting the beef brown. Using a spatula, stir meat, cooking another 2 minutes. Transfer to a plate.",
+        "Add remaining teaspoon of oil to wok, add peppers and onions and cook about 4 to 5 minutes. Return beef to the wok, add the sauce and red pepper flakes and stir fry about 30 seconds until slightly thickened."
+      ]
+    },
+    {
+      user: "johnnicholas",
+      recipename: "Spicy Shrimp Fried Rice",
+      picture:
+        "https://www.skinnytaste.com/wp-content/uploads/2009/06/Spicy-Shrimp-Fried-Rice-3.jpg",
+      ingredients: [
+        "3 cups cooked brown rice",
+        "1lb large shrimp, peeled and deveined",
+        "1/2 tsp chili powder",
+        "1/2 tsp paprika",
+        "1/8 tsp cayenne pepper, or more to taste",
+        "2 large egg whites, beaten",
+        "1 large whole egg, beaten",
+        "1/2 medium onion, chopped",
+        "2 cloves of garlic, diced",
+        "5 medium scallions, chopped, whites and greens separated",
+        "spray oil",
+        "1 tbsp sesame oil",
+        "1 tsp crushed red pepper flakes, or more to taste",
+        "4 tsp soy sauce, or more to taste",
+        "1 tsp Asian fish sauce, (or more soy sauce)"
+      ],
+      instructions: [
+        "In a bowl, season shrimp with cayenne pepper, chili powder, paprika, salt, pepper and garlic powder.",
+        "In a hot wok, over medium high heat, spray a little oil and cook the eggs, when cooked, remove from pan and set aside, 1 to 2 minutes.",
+        "Increase the heat to high, when hot add the sesame oil and saute onions, scallion whites, garlic and hot pepper flakes for about 1-2 minutes.",
+        "Add shrimp and saute until no longer translucent, 2 to 3 minutes.",
+        "Add rice and leave in the skillet to let the bottom brown, 2 to 3 minutes, stir and add the egg, soy sauce and fish sauce, mixing well for about 2 minutes.",
+        "Add greens of the scallions and serve."
+      ]
+    },
+    {
+      user: "praveeniscool",
+      recipename: "Chicken Zucchini Stir Fry",
+      picture:
+        "https://www.skinnytaste.com/wp-content/uploads/2019/08/Chicken-and-Zucchini-Stir-Fry-RS-5.jpg",
+      ingredients: [
+        "1/4 cup low sodium soy sauce or use gf soy sauce",
+        "1 cup chicken broth",
+        "1 tbsp cornstarch",
+        "2 tbsp mirin",
+        "1 tbsp sugar",
+        "2 tsp sesame oil",
+        "1 tbsp canola oil, divided",
+        "1 tbsp minced garlic",
+        "1 tbsp minced ginger",
+        "1lb chicken breast, sliced very thin",
+        "2 cups zucchini, cut 1/4 inch thick half moons (1 large zucchini)",
+        "sesame seeds and scallion for garnish, if desired"
+      ],
+      instructions: [
+        "In a large bowl add the soy sauce, chicken broth, cornstarch, mirin, sugar, and sesame oil and whisk until everything is completely dissolved.",
+        "In a large skillet add one teaspoon canola oil on medium high heat and cook half the chicken until just cooked through, about 2-3 minutes on each side. Set aside on a plate.",
+        "Repeat with the second half of the chicken and an additional teaspoon of oil. Remove the chicken to the plate.",
+        "Add in the remaining 1 teaspoon oil, garlic and ginger and cook for 30-45 seconds until very fragrant but not browned.",
+        "Stir the garlic and ginger well and add in the sauce, whisking well. Cook the sauce 1 minute, then add in the zucchini and cook for 2 minutes more, until thickened and the zucchini is tender crisp. Remove from heat, add in the chicken and stir well to coat. Garnish with sesame seeds and scallions if desired."
+      ]
+    },
+    {
+      user: "sashafierce",
+      recipename: "Cilantro Lime Fish Tacos",
+      picture:
+        "https://www.skinnytaste.com/wp-content/uploads/2012/02/fish-taco-5.jpg",
+      ingredients: [
+        "1lb fish fillets, rinsed and pat dried (flounder, tilapia, etc.)",
+        "1 tsp olive oil",
+        "1 small onion, chopped",
+        "4 garlic cloves, minced",
+        "2 jalapeno peppers, chopped (seeds removed for less heat",
+        "2 cups diced tomatoes",
+        "1/4 cup fresh cilantro, chopped",
+        "3 tbsp lime juice",
+        "kosher salt and pepper to taste",
+        "8 white corn tortillas",
+        "4oz (1 medium) haas avocado, sliced",
+        "1 cup sliced cabbage",
+        "lime wedges and cilantro for garnish"
+      ],
+      instructions: [
+        "Heat olive oil in a large skillet over medium heat.",
+        "Sauté onion until translucent about 4 to 5 minutes, then add garlic and cook 1 minute more. Mix well.",
+        "Place fish on the skillet and cook until the flesh starts to flake.",
+        "Add jalapeño peppers, tomatoes, cilantro and lime juice. Sauté over medium-high heat for about 5 minutes, breaking up the fish with the spoon to get everything mixed well; season to taste with salt and pepper.",
+        "Meanwhile, heat tortillas on a skillet or directly over the flame of the burner a minute or 2 on each side to warm.",
+        "Serve a little over 1/4 cup of fish on each warmed tortilla topped with cabbage, avocado, cilantro, and a squeeze of lime."
+      ]
+    },
+    {
+      user: "diggityrocks",
+      recipename: "Garlic Lovers Roast Beef",
+      picture:
+        "https://www.skinnytaste.com/wp-content/uploads/2013/06/Garlic-Lover%E2%80%99s-Roast-Beef-7.jpg",
+      ingredients: [
+        "2-3lb roast or eye round, all fat trimmed off",
+        "3-4 cloves garlic, cut into thin slivers",
+        "olive oil spray",
+        "kosher salt, to taste",
+        "fresh cracked pepper, to taste",
+        "2 tsp dried chopped rosemary",
+        "meat thermometer"
+      ],
+      instructions: [
+        "Remove the roast from refrigerator 1 hour before cooking to reach room temperature. Trim all the fat off the meat. Using a sharp knife, pierce meat about 1/2-inch deep and insert slivers of garlic in each hole, pushing all the way in. Lightly spray the meat with olive oil and season generously with salt, pepper and rosemary. Place the thermometer all the way into the center of the meat.",
+        "Preheat oven to 350F. When the oven reaches 350F, place the roast in a roasting pan and put in the oven.",
+        "Roast until the thermometer reads 130° for rare, 140° for medium rare, 150° for medium, and 155-160° for well done. Remove the roast from the oven and let it rest 10-20 minutes before you cut it so that the juices distributed evenly. I personally remove my roast beef from the oven when it is 135° for medium rare. The temperature will rise an additional 5 degrees as it sits.",
+        "Slice thin and serve."
+      ]
+    }
+  ]
+};
---
+++ src/MockData/MockUsers.js
@@ -0,0 +1,34 @@
+export default {
+  users: [
+    {
+      firstname: "Ella",
+      lastname: "Horn",
+      username: "ellahorn",
+      password: "password"
+    },
+    {
+      firstname: "John",
+      lastname: "Nicholas",
+      username: "johnnicholas",
+      password: "hello"
+    },
+    {
+      firstname: "Praveen",
+      lastname: "Kumar",
+      username: "praveeniscool",
+      password: "ilikefood"
+    },
+    {
+      firstname: "Sasha",
+      lastname: "Fierce",
+      username: "sashafierce",
+      password: "dogs123"
+    },
+    {
+      firstname: "Hot Diggity",
+      lastname: "Dog",
+      username: "diggityrocks",
+      password: "bananas"
+    }
+  ]
+};
