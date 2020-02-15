dca9216ceef69b39a482a0974ded48d303e3629d
Ella Horn <ellalouisahorn@yahoo.com>
2020-02-15 13:14:12 -0500

    added login form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <form action="">
+        <ul>
+          <li>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" />
+          </li>
+          <li>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+          </li>
+        </ul>
+        <button type="submit">Log in</button>
+      </form>
+      <h3>
+        Don't have an account yet? Create one <Link to="/signup">here</Link>
+      </h3>
+    </div>
+  );
 };
 
 export default Login;
