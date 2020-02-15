db1afa6cfc8f0b40ec8199563753e78fc7ceef4a
Ella Horn <ellalouisahorn@yahoo.com>
2020-02-15 16:04:46 -0500

    added profile pictures to mock data

--- src/MockData/MockUsers.js
+++ src/MockData/MockUsers.js
@@ -4,31 +4,41 @@ export default {
       firstname: "Ella",
       lastname: "Horn",
       username: "ellahorn",
-      password: "password"
+      password: "password",
+      profilepicture:
+        "https://magazine.washington.edu/columns_wordpress/wp-content/uploads/2018/06/Narwhal375.jpg"
     },
     {
       firstname: "John",
       lastname: "Nicholas",
       username: "johnnicholas",
-      password: "hello"
+      password: "hello",
+      profilepicture:
+        "https://www.nationalgeographic.com/content/dam/yourshot/2014/03/3143130.jpg"
     },
     {
       firstname: "Praveen",
       lastname: "Kumar",
       username: "praveeniscool",
-      password: "ilikefood"
+      password: "ilikefood",
+      profilepicture:
+        "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_weight_other/1800x1200_cat_weight_other.jpg?resize=600px:*"
     },
     {
       firstname: "Sasha",
       lastname: "Fierce",
       username: "sashafierce",
-      password: "dogs123"
+      password: "dogs123",
+      profilepicture:
+        "https://vetstreet.brightspotcdn.com/dims4/default/4e5ffb4/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F04%2Fa08210a8c011e0a0d50050568d634f%2Ffile%2Fminiature-schnauzer-5-645mk070411.jpg"
     },
     {
       firstname: "Hot Diggity",
       lastname: "Dog",
       username: "diggityrocks",
-      password: "bananas"
+      password: "bananas",
+      profilepicture:
+        "https://i.pinimg.com/736x/87/ac/9d/87ac9dcf3919987eb20946c723a96483.jpg"
     }
   ]
 };
