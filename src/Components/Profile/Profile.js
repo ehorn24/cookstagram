b7a73c706c4c286b39b0396e2091dcd245c2e630
Ella Horn <ellalouisahorn@yahoo.com>
2020-02-15 16:12:24 -0500

    profile component

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,7 +1,32 @@
 import React from "react";
+import MockUsers from "../../MockData/MockUsers";
+import MockRecipes from "../../MockData/MockRecipes";
 
-const Profile = () => {
-  return <div></div>;
+const Profile = ({ match }) => {
+  let currentProfile = MockUsers.users.filter(
+    user => user.username === match.params.username
+  );
+  let userRecipes = MockRecipes.recipes.filter(
+    recipe => recipe.user === match.params.username
+  );
+  let { firstname, lastname, username, profilepicture } = currentProfile[0];
+
+  return (
+    <main>
+      <section className="sidebar">
+        <h1>
+          {firstname} {lastname}
+        </h1>
+        <h2>{username}</h2>
+        <img src={profilepicture} alt="profile" />
+      </section>
+      <section className="user-recipes">
+        {userRecipes.map(recipe => {
+          return <div>{recipe.recipename}</div>;
+        })}
+      </section>
+    </main>
+  );
 };
 
 export default Profile;
