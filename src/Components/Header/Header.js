e461bd3ff7340020172b60216734df611c539d3b
Ella Horn <ellalouisahorn@yahoo.com>
2020-02-15 13:00:26 -0500

    moved signup and log in buttons to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -8,8 +9,12 @@ const Header = () => {
         The social network dedicated to the one thing that really matters -
         food, and how to make it.
       </p>
-      <button>Sign up</button>
-      <button>Log in</button>
+      <Link to="/signup">
+        <button>Sign up</button>
+      </Link>
+      <Link to="/login">
+        <button>Log in</button>
+      </Link>
     </header>
   );
 };
